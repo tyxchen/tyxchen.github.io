67358adaea6aa8f42fee951af18db7672c18f042
Terry Chen <tyxchen@outlook.com>
2026-03-28 00:26:19 -0400

    add mdx, svelte, math plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 
+import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import svelte from '@astrojs/svelte';
+
 const SITE = Deno.env.get('SITE');
 
 export default defineConfig({
@@ -10,9 +15,22 @@ export default defineConfig({
     host: true,
   },
   output: 'static',
-  integrations: [icon()],
+  integrations: [icon(), mdx(), svelte()],
+  markdown: {
+    remarkPlugins: [
+      [remarkMath, { singleDollarTextMath: false }],
+    ],
+    rehypePlugins: [rehypeKatex],
+  },
   //prefetch: true,
   redirects: {
     '/archive': '/posts',
-  }
+  },
+  vite: {
+    server: {
+      watch: {
+        ignored: ["vendor/**/*"],
+      },
+    },
+  },
 });
