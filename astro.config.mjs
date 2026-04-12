58a230b606dae584fee6dcac33f59e84709d73c8
Terry Chen <tyxchen@outlook.com>
2026-04-12 14:59:32 -0400

    shiki colour tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,9 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import svelte from '@astrojs/svelte';
 
-const SITE = Deno.env.get('SITE');
+const SITE = Deno.env.get('SITE') ?? 'https://localhost:4321';
+
+const slangGrammar = (await fetch("https://raw.githubusercontent.com/shader-slang/slang-vscode-extension/refs/heads/main/syntaxes/slang.tmLanguage.json")).json();
 
 export default defineConfig({
   base: '/',
@@ -22,10 +24,13 @@ export default defineConfig({
     ],
     rehypePlugins: [rehypeKatex],
     shikiConfig: {
+      langs: [slangGrammar],
+      langAlias: { slang: 'Slang' },
       themes: {
         light: 'one-light',
         dark: 'one-dark-pro',
       },
+      defaultColor: false,
     },
   },
   //prefetch: true,
